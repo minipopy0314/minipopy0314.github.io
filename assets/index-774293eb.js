5fd636841d7346bc0c212a3b4f59310cc66673b4
aries <aries.chou@collaborate.tw>
2023-08-29 18:08:33 +0800

    Updates

--- assets/index-774293eb.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-bea98dec.js";const r=()=>t.jsx("div",{children:"test"});export{r as default};
---
+++ assets/index-d23c4ee5.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-7b6b2fe6.js";const e=()=>r.jsx("div",{children:"404"});export{e as default};
